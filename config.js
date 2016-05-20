a333cabfdf6ac4f1a53eb112b8cacc67b9ad04dd
Loulou <gitzingerlouison@gmail.com>
2016-05-20 15:53:26 +0200

    je répare les bêtises de Mathieu

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secret': 'antoineandlouison',
-    'database': 'mongodb://louison:123456@ds037395.mongolab.com:37395/homecontrol'
+    'database': 'mongodb://louison:123456@ds037395.mongolab.com:37395/homecontrol',
+    "knx": {
+        "ipplateauknx": "192.168.1.118",
+        "portplateauknx": 3671,
+        "portserver": 13671
+    }
 };
